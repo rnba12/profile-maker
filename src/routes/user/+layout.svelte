a19d464e5cc2cd45c709f1a0a9df78846c917fcc
rnba12 <rnba12@yahoo.co.uk>
2023-05-02 01:52:12 +0100

    style: nav bar

--- src/routes/user/+layout.svelte
+++ src/routes/user/+layout.svelte
@@ -1,10 +1,11 @@
 <script>
     import { signOut } from "@auth/sveltekit/client";
 </script>
-<nav class="flex flex-row">
-        <a href="/user/edit">Edit</a>
-        <a href="/user/settings">Settings</a>
-        <button on:click={() => signOut()}>Sign Out</button>
+
+<nav class="flex flex-row gap-1 justify-center">
+        <a class="font-bold border rounded-full p-2 hover:bg-slate-200" href="/user/edit">Edit</a>
+        <a class="font-bold border rounded-full p-2 hover:bg-slate-200" href="/user/settings">Settings</a>
+        <button class="text-red-600 font-bold border rounded-full p-2 hover:bg-slate-200" on:click={() => signOut()}>Sign Out</button>
 </nav>
 
 <slot/>
