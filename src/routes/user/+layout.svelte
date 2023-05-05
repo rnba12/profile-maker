889724cf63d275c9536b314132393614da212e4e
rnba12 <rnba12@yahoo.co.uk>
2023-05-05 13:02:32 +0100

    feat: add projects page

--- src/routes/user/+layout.svelte
+++ src/routes/user/+layout.svelte
@@ -4,6 +4,7 @@
 
 <nav class="flex flex-row gap-1 justify-center">
         <a class="font-bold border rounded-full p-2 hover:bg-slate-200" href="/user/edit">Edit</a>
+        <a class="font-bold border rounded-full p-2 hover:bg-slate-200" href="/user/projects">Projects</a>
         <a class="font-bold border rounded-full p-2 hover:bg-slate-200" href="/user/settings">Settings</a>
         <button class="text-red-600 font-bold border rounded-full p-2 hover:bg-slate-200" on:click={() => signOut()}>Sign Out</button>
 </nav>
