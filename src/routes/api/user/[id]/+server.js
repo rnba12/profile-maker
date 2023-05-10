8fd289802678bcfd544b7427043feaa33dda7a2c
rnba12 <rnba12@yahoo.co.uk>
2023-05-10 17:12:48 +0100

    fix: remove api route

--- src/routes/api/user/[id]/+server.js
+++
@@ -1,9 +0,0 @@
-import { json } from "@sveltejs/kit"
-import { dbConnect } from "$lib/db/db"
-import { dbDisconnect } from "$lib/db/db"
-
-export const GET = async () => {
-    await dbConnect();
-    await dbDisconnect();
-    return json({message: "user"},{ status: 201})
-}
