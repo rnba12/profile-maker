fcbaed3c11b3e819b6a50743cdadb8ebfdce30db
rnba12 <rnba12@yahoo.co.uk>
2023-05-14 21:05:59 +0100

    style: settings page

--- src/routes/user/settings/+page.svelte
+++ src/routes/user/settings/+page.svelte
@@ -2,6 +2,7 @@
     import { page } from '$app/stores';
     import { enhance, applyAction } from '$app/forms';
     import { invalidateAll } from '$app/navigation';
+    import Module from '$lib/components/Module.svelte';
 
     $: linkName = $page.data.profile.linkName;
 
@@ -19,18 +20,45 @@
 
 </script>
 
-<h1>Settings</h1>
+<div class="settings-page">
+    <h1>Settings</h1>
+    
+    <Module header="Change Profile Link">
+        <form method="post" action="?/editLink" use:enhance={handleUpdate}>
+            <label for="linkName">Link Name</label>
+            <input type="text" name="linkName" value={linkName}>
+            <button>Update</button>
+            {#if $page.form}
+                <p class:error={$page.form.error} class:success={$page.form.success}>{$page.form.message}</p>
+            {/if}
+        </form>
+    </Module>
+    
+    <Module header="Delete Account">
+        <form method="post" action="?/deleteAccount">
+            <button class="delete" type="submit">Delete Account</button>
+            <p>Warning: This action is irreversible. Only proceed if you are confident you want to delete your account</p>
+        </form>
+    </Module>
+</div>
 
-<form method="post" action="?/editLink" use:enhance={handleUpdate}>
-    {#if $page.form}
-        {$page.form.message}
-    {/if}
-    <label for="linkName">Link Name</label>
-    <input type="text" name="linkName" value={linkName}>
-    <button>Update</button>
-</form>
-
-<form method="post" action="?/deleteAccount">
-    <button type="submit">Delete Account</button>
-    <p>Warning: This action is irreversible. Only proceed if you are confident you want to delete your account</p>
-</form>
+<style>
+    .settings-page {
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+    }
+    h1 {
+        font-size: 3rem;
+        line-height: 0;
+    }
+    .error {
+        color: red;
+    }
+    .success {
+        color: green;
+    }
+    .delete {
+        color: red;
+    }
+</style>
