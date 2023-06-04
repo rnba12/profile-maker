b1e0f61854edd0a37533dbc900e9fc3b8479e506
rnba12 <rnba12@yahoo.co.uk>
2023-06-04 23:12:51 +0100

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,11 @@
+<script>
+    import { page } from '$app/stores';
+</script>
+
+
+<h1>There's been a Problem😭</h1>
+<h1>Error: {$page.status} - {$page.error.message}</h1>
+
+<a href="/">Back To Home</a>
+
+<p>Found an issue? <a href="https://www.github.com/rnba12/profile-maker/issues">Let me now</a></p>
