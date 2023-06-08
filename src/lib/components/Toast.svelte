4b01a6b0da11d6e3d38fd90dfbb8cc0b86bc18fe
rnba12 <rnba12@yahoo.co.uk>
2023-06-09 00:17:47 +0100

    style: toast

--- src/lib/components/Toast.svelte
+++ src/lib/components/Toast.svelte
@@ -1,8 +1,7 @@
 <script>
-    import { createEventDispatcher, onMount } from "svelte";
+    import { onMount } from "svelte";
     import { fade } from "svelte/transition";
 
-	let dispatch = createEventDispatcher()
     export let message;
     export let success = true;
 
@@ -17,50 +16,45 @@
 </script>
 
 {#if message}    
-<div class="toast-wrapper">
     <div class="toast" class:fail={!success} transition:fade>
-        <p>{message}</p>
+        <p><iconify-icon icon="material-symbols:info-outline"></iconify-icon>{message}</p>
         <button class="x-btn" on:click={handleClose}><iconify-icon icon="ph:x"></iconify-icon></button>
     </div>
-</div>
 {/if}
 
 <style lang="scss">
     .fail {
         background-color: rgb(228, 160, 160) !important;
-    }
-    .toast-wrapper {
-        z-index: 1;
-        position: fixed;
-        bottom: 0;
-        left: 0;
-        right: 0;
-        width: auto;
-        height: 200px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
+        border-color: rgb(186, 134, 134) !important;
     }
 
     .toast {
-        font-weight: 600;
-        font-size: 1.1rem;
-        position: relative;
-        z-index: 2;
-        min-width: 400px;
-        background-color: greenyellow;
-        // border: 1px solid green;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        border-radius: 10px;
+        position: fixed;
+        bottom: 4rem;
+        left: 0;
+        right: 0;
+        z-index: 999;
+        font-weight: 500;
+        width: 400px;
+        background-color: rgb(168, 250, 141);
+        border: 2px solid rgb(151, 223, 126);
+        margin: auto;
+        border-radius: 8px;
         display: grid;
         grid-template-columns: 80% 20%;
-        text-align: center;
+        
         button {
+            display: inline-flex;
+            align-items: center;
             background-color: inherit;
-            font-size: 1.3rem;
+            font-size: 1.3em;
             width: min-content;
+        }
+        p {
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            gap: 5px;
         }
     }
 </style>
